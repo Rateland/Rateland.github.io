48c5377f605afa3d86323371ba1b474840b9cb03
Rateland <134687101+Rateland@users.noreply.github.com>
2024-02-03 11:49:47 +0100

    Ajout d’une seconde page +CSS +JS

---
+++ script-mon-espace.js
@@ -0,0 +1,31 @@
+document.getElementById('identification-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    // Validation des informations (exemple : vérification de l'âge)
+    var birthdate = new Date(document.getElementById('birthdate').value);
+    var currentDate = new Date();
+    var age = currentDate.getFullYear() - birthdate.getFullYear();
+
+    if (age < 21) {
+        showNotification("Vous êtes mineur. Veuillez vous identifier avec les données parentales et aller dans l'onglet 'Informations familiales'.");
+    } else {
+        // Réinitialiser la notification si précédemment affichée
+        hideNotification();
+        
+        // Vérification supplémentaire des autres données si nécessaire
+        // ...
+        
+        // Soumission du formulaire si toutes les vérifications sont réussies
+        alert('Formulaire soumis avec succès!');
+    }
+});
+
+function showNotification(message) {
+    var notificationElement = document.getElementById('notification');
+    notificationElement.innerText = message;
+    notificationElement.style.display = 'block';
+}
+
+function hideNotification() {
+    document.getElementById('notification').style.display = 'none';
+}
