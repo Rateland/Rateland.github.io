0b68591f61097cdc53edd13af6b8ca973fde8b20
Rateland <134687101+Rateland@users.noreply.github.com>
2024-02-05 08:47:15 +0100

    Pleins d’ajouts

--- script-mon-espace.js
+++ script-mon-espace.js
@@ -29,3 +29,19 @@ function showNotification(message) {
 function hideNotification() {
     document.getElementById('notification').style.display = 'none';
 }
+
+window.addEventListener("scroll", function () {
+    if (document.documentElement.scrollTop > 100) {
+      document.getElementById("scroll-top").style.display = "block";
+    } else {
+      document.getElementById("scroll-top").style.display = "none";
+    }
+  });
+  
+  document.getElementById("scroll-top").addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+  
